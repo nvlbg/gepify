417be60cd684fef3d51c8985ab48cc0693d1b167
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2016-05-23 16:57:36 +0300

    some test improvements

--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -6,11 +6,12 @@ from urllib import parse
 from pprint import pprint
 from unittest import mock
 from gepify.services import spotify
+import json
 
 
 class MockResponse:
     def __init__(self, data, status_code):
-        self.text = str(data)
+        self.text = json.dumps(data)
         self.status_code = status_code
 
 
@@ -27,17 +28,23 @@ class ProfileMixin():
     def login(self):
         login_response = self.client.get(url_for('spotify.login'))
         spotify_redirect = login_response.location
+        self.assertTrue(spotify_redirect.startswith('https://accounts.spotify.com/authorize'))
+
         params = parse.parse_qs(parse.urlparse(spotify_redirect).query)
 
-        return self.client.get(url_for('spotify.callback'), data={
-            'code': 'dummy code',
-            'state': params['state'][0]
-        })
+        response = self.client.get(url_for(
+            'spotify.callback', code='dummy code', state=params['state'][0]
+        ))
+        self.assertRedirects(response, url_for('spotify.index'))
+        return response
 
     def logout(self):
-        return self.client.get(url_for('spotify.logout'))
+        logout_response = self.client.get(url_for('spotify.logout'))
+        self.assertRedirects(logout_response, url_for('index'))
+        return logout_response
 
 
+@mock.patch('requests.post', side_effect=mocked_spotify_api)
 class SpotifyDecoratorsTestCase(TestCase, ProfileMixin):
     def create_app(self):
         gepify.app.config['TESTING'] = True
@@ -46,7 +53,7 @@ class SpotifyDecoratorsTestCase(TestCase, ProfileMixin):
     def setUp(self):
         self.client = gepify.app.test_client()
 
-    def test_login_required_decorator(self):
+    def test_login_required_decorator(self, post):
         @self.app.route('/test')
         @spotify.view_decorators.login_required
         def test():
@@ -55,7 +62,7 @@ class SpotifyDecoratorsTestCase(TestCase, ProfileMixin):
         response = self.client.get('/test')
         self.assertRedirects(response, url_for('spotify.login'))
 
-        self.login()
+        login_response = self.login()
 
         response = self.client.get('/test')
         self.assert200(response)
@@ -67,21 +74,22 @@ class SpotifyDecoratorsTestCase(TestCase, ProfileMixin):
         self.assertRedirects(response, url_for('spotify.login'))
 
 
+@mock.patch('requests.post', side_effect=mocked_spotify_api)
 class SpotifyTestCase(TestCase, ProfileMixin):
     def create_app(self):
         gepify.app.config['TESTING'] = True
+        gepify.app.config['PRESERVE_CONTEXT_ON_EXCEPTION'] = False
         return gepify.app
 
     def setUp(self):
         self.client = gepify.app.test_client()
 
-    def test_index_if_not_logged_in(self):
+    def test_index_if_not_logged_in(self, post):
         response = self.client.get(url_for('spotify.index'))
         self.assertRedirects(response, url_for('spotify.login'))
 
-    # @mock.patch('requests.post',
-    #             side_effect=mocked_spotify_api)
-    # def test_login(self, post):
-    #     response = self.client.get(url_for('spotify.login'))
-    #     login = self.login()
-    #     self.assertIn(b'Error: did not get token', login.data)
+    # def test_index_if_logged_in(self, post):
+    #     self.login()
+    #     response = self.client.get(url_for('spotify.index'))
+    #     print(response.data)
+    #     pass

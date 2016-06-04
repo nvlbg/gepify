74f490ea228f6bb06602b31ca2f89324ea2b9da5
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2016-06-04 17:36:22 +0300

    fixes Procfile after the refactoring

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -b 0.0.0.0:$PORT gepify:app
+web: gunicorn -b 0.0.0.0:$PORT runserver:app
--- runserver.py
+++ runserver.py
@@ -1,4 +1,6 @@
 from gepify import create_app
 
+app = create_app()
+
 if __name__ == "__main__":
-    create_app().run()
+    app.run()

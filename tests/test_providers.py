0b540b132e18ba175db841510255a68482764279
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2016-07-03 20:53:21 +0300

    adds some unit tests for provider tasks

--- tests/test_providers.py
+++ tests/test_providers.py
@@ -3,31 +3,22 @@ import gepify
 from gepify.providers import songs, playlists, youtube, soundcloud
 from werkzeug.contrib.cache import SimpleCache
 import json
+import time
 
 
 class SongsTestCase(TestCase):
-    def setUp(cls):
+    def setUp(self):
         songs.cache = SimpleCache()
 
-    @mock.patch('gepify.providers.songs.cache.get',
-                side_effect=lambda song: None)
-    @mock.patch('gepify.providers.songs.cache.set')
-    def test_get_song_if_song_is_not_in_cache(self, cache_set, cache_get):
+    def test_get_song_if_song_is_not_in_cache(self):
         song = songs.get_song('some song')
         self.assertEqual(song['name'], 'some song')
         self.assertEqual(song['files'], {})
-        self.assertTrue(cache_get.called)
-        self.assertTrue(cache_set.called)
 
-    @mock.patch('gepify.providers.songs.cache.get',
-                side_effect=lambda song: {'name': song, 'files': {}})
-    @mock.patch('gepify.providers.songs.cache.set')
-    def test_get_song_if_song_is_in_cache(self, cache_set, cache_get):
+    def test_get_song_if_song_is_in_cache(self):
         song = songs.get_song('some song')
         self.assertEqual(song['name'], 'some song')
         self.assertEqual(song['files'], {})
-        self.assertTrue(cache_get.called)
-        self.assertFalse(cache_set.called)
 
     def test_add_song_file_if_format_is_unsupported(self):
         with self.assertRaises(ValueError):
@@ -53,8 +44,59 @@ class SongsTestCase(TestCase):
         self.assertFalse(songs.has_song_format('some song', 'mp3'))
 
 
+class SongsTasksTestCase(TestCase):
+    def setUp(self):
+        songs.cache = SimpleCache()
+
+    def test_download_song_in_unsupported_format(self):
+        with self.assertRaisesRegex(ValueError, 'Format not supported: wav'):
+            songs.download_song('song', format='wav')
+
+    @mock.patch('logging.Logger.info')
+    def test_download_song_if_song_is_already_downloaded(self, log_info):
+        songs.add_song_file('song', 'song.mp3', 'mp3')
+        songs.download_song('song', format='mp3')
+        log_info.assert_called_with(
+            'Attempt to download already downloaded song.'
+            'Cache: song.mp3'
+        )
+
+    @mock.patch('logging.Logger.info')
+    def test_download_song_if_song_is_being_downloaded(self, log_info):
+        song = songs.get_song('song')
+        song['files']['mp3'] = 'downloading'
+        songs.cache.set('song', song)
+        songs.download_song('song', format='mp3')
+        log_info.assert_called_with(
+            'Attempt to download a song in the process of downloading')
+
+    def test_download_song_with_unsupported_provider(self):
+        with self.assertRaisesRegex(ValueError, 'Provider not found: zamunda'):
+            songs.download_song('song', provider='zamunda')
+        song = songs.get_song('song')
+        self.assertNotIn('mp3', song['files'])
+
+    @mock.patch('gepify.providers.youtube.get_song_id',
+                side_effect=lambda name: 'dQw4w9WgXcQ')
+    @mock.patch('gepify.providers.youtube.download_song')
+    def test_download_song_with_youtube(self, download_song, *args):
+        songs.download_song('song')
+        download_song.assert_called_with('dQw4w9WgXcQ', 'mp3')
+        song = songs.get_song('song')
+        self.assertEqual(song['files']['mp3'], 'songs/dQw4w9WgXcQ.mp3')
+
+    @mock.patch('gepify.providers.soundcloud.get_song_id',
+                side_effect=lambda name: (1234, 'song id'))
+    @mock.patch('gepify.providers.soundcloud.download_song')
+    def test_download_song_with_soundcloud(self, download_song, *args):
+        songs.download_song('song', provider='soundcloud')
+        download_song.assert_called_with('song id', 'mp3')
+        song = songs.get_song('song')
+        self.assertEqual(song['files']['mp3'], 'songs/1234.mp3')
+
+
 class PlaylistsTestCase(TestCase):
-    def setUp(cls):
+    def setUp(self):
         playlists.cache = SimpleCache()
 
     @mock.patch('gepify.providers.playlists.cache.get')
@@ -76,6 +118,34 @@ class PlaylistsTestCase(TestCase):
         playlists.cache.set('spotify_some playlist_mp3', 'downloading')
         self.assertFalse(
             playlists.has_playlist('spotify', 'some playlist', 'mp3'))
+
+
+def mocked_list_dir(dir):
+    return ['1.zip', '2.zip']
+
+
+def mocked_getmtime(file):
+    if file == 'playlists/1.zip':
+        return time.time() - 60*60
+    if file == 'playlists/2.zip':
+        return time.time() - 42
+
+
+class PlaylistsTasksTestCase(TestCase):
+    def setUp(self):
+        playlists.cache = mock.MagicMock()
+
+    def tearDown(self):
+        playlists.cache.reset_mock()
+
+    @mock.patch('os.listdir', side_effect=mocked_list_dir)
+    @mock.patch('os.path.getmtime', side_effect=mocked_getmtime)
+    @mock.patch('logging.Logger')
+    @mock.patch('os.remove')
+    def test_clean_playlists(self, os_remove, *args):
+        playlists.clean_playlists()
+        self.assertEqual(os_remove.call_count, 1)
+        os_remove.assert_called_with('playlists/1.zip')
 
 
 class mocked_Resource():

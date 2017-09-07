8feec4c023e6725d462e8f067e6fb2a696211a6b
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2017-09-07 22:59:57 +0300

    Add influxdb support

---
+++ gepify/influxdb.py
@@ -0,0 +1,17 @@
+"""InfluxDB configuration.
+
+This module serves as a configuration for a InfluxDB client
+used for collecting various statistics and metrics.
+"""
+
+from influxdb import InfluxDBClient
+import os
+
+host = os.environ.get('INFLUXDB_HOST', 'localhost')
+port = os.environ.get('INFLUXDB_PORT', 8086)
+username = os.environ.get('INFLUXDB_USER', 'root')
+password = os.environ.get('INFLUXDB_PASSWORD', 'root')
+database = os.environ.get('INFLUXDB_DATABASE', 'gepify')
+
+client = InfluxDBClient(host, port, username, password, database)
+
--- gepify/views.py
+++ gepify/views.py
@@ -1,8 +1,16 @@
 from flask import Blueprint, render_template
+from gepify.influxdb import client as influxdb
 
 views = Blueprint('views', __name__)
 
 
 @views.route('/')
 def index():
+    influxdb.write({
+        'measurement': 'index_page_loads',
+        'fields': {
+            'value': 1
+        }
+    })
+
     return render_template('index.html')
